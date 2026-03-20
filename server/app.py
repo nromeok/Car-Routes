e52c3a38a3de51e8399a6861c22cf798694324ac
nromeok <nelsonromeook@gmail.com>
2026-03-21 00:01:31 +0300

    Added Routing to app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"Welcome to Flatiron Cars"
+
+@app.route("/<model>")
+def show_model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
